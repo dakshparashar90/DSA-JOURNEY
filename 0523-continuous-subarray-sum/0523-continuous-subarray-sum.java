d4e7702e00c965d3abee55f2e41a3535401a197d
dakshparashar90 <155139713+dakshparashar90@users.noreply.github.com>
2026-07-24 12:32:27 +0530

    Time: 24 ms (63.26%), Space: 139.8 MB (32.82%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,29 +1,62 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-        HashMap<Integer,Integer>mpp=new HashMap<>();
-        int sum=0;
-        int i=0;
-       
-       while(i<nums.length){
-            sum+=nums[i];
-            int mod=sum%k;
-            
-            if(sum%k==0 &&  i>=1){
+
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            sum += nums[i];
+
+            int mod = sum % k;
+
+            if (mod == 0 && i >= 1) {
                 return true;
-
-            }
-            else if(mpp.containsKey(mod)){
-                    int vl=mpp.get(mod);
-                    if(i-vl>=2){
-                        return true;
-                    }
-            }
-            else{
-                mpp.put(mod,i);
             }
 
-            i++;
+            if (mpp.containsKey(mod)) {
+                int idx = mpp.get(mod);
+
+                if (i - idx >= 2) {
+                    return true;
+                }
+            } else {
+                mpp.put(mod, i);
+            }
         }
+
         return false;
     }
 }
+
+
+// class Solution {
+//     public boolean checkSubarraySum(int[] nums, int k) {
+//         HashMap<Integer,Integer>mpp=new HashMap<>();
+//         int sum=0;
+//         int i=0;
+       
+//        while(i<nums.length){
+//             sum+=nums[i];
+//             int mod=sum%k;
+            
+//             if(sum%k==0 &&  i>=1){
+//                 return true;
+
+//             }
+//             else if(mpp.containsKey(mod)){
+//                     int vl=mpp.get(mod);
+//                     if(i-vl>=2){
+//                         return true;
+//                     }
+//             }
+//             else{
+//                 mpp.put(mod,i);
+//             }
+
+//             i++;
+//         }
+//         return false;
+//     }
+// }
