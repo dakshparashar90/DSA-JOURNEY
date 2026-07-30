a91432b34d84e7df4b67a853e0332cfd3a1ab7a3
dakshparashar90 <155139713+dakshparashar90@users.noreply.github.com>
2026-07-30 23:39:50 +0530

    Time: 1 ms (91.36%), Space: 45.2 MB (38.51%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,34 +1,49 @@
+class DisjointSet{
+    int prnt[];
+    int size[];
+    DisjointSet(int n){
+        prnt=new int[n+1];
+        size=new int[n+1];
+        for(int i=1;i<=n;i++){
+            prnt[i]=i;
+            size[i]=1;
+        }
+    }
+    public int findPar(int u){
+        if(prnt[u]==u) return u;
+        return prnt[u]=findPar(prnt[u]);
+    }
+    public void unionBySize(int u,int v){
+        int pu=findPar(u);
+        int pv=findPar(v);
+        if(pu==pv) return;
+        if(size[pu]>size[pv]){
+            size[pu]+=size[pv];
+            prnt[pv]=pu;
+        }
+        else{
+            size[pv]+=size[pu];
+            prnt[pu]=pv;
+        }
+    }
+}
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
-        ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
-
-        for(int i=0;i<=edges.length;i++){
-            adj.add(new ArrayList<>());
-        }
-        boolean vis[]=new boolean[edges.length+1];
-       
-        for(int arr[]:edges){
-            int u=arr[0];
-            int v=arr[1];
-
-            if(check(u,v,adj,vis))return arr;
-          
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-
-
-        }
-        return new int[]{};
-    }
-    public boolean check(int src,int tar,ArrayList<ArrayList<Integer>>adj, boolean vis[]){
-       if(src==tar)return true;
-        vis[src]=true;
-        for(int it:adj.get(src)){
-            if(!vis[it]){
-                 if(check(it, tar, adj, vis)) return true;
+        int n=edges.length;
+        DisjointSet ds=new DisjointSet(n);
+        int ans[]=new int[2];
+        for(int i=0;i<n;i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            if(ds.findPar(u)==ds.findPar(v)){
+                ans[0]=u;
+                ans[1]=v;
+                break;
+            }
+            else{
+                ds.unionBySize(u,v);
             }
         }
-        vis[src]=false;
-        return false;
+        return ans;
     }
 }
