4facd166d1b07ffb8901d5f0bc0d12f296ba95c1
dakshparashar90 <155139713+dakshparashar90@users.noreply.github.com>
2026-09-04 17:56:01 +0530

    Time: 25 ms (77.65%), Space: 44.3 MB (40.84%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -1,79 +1,42 @@
-// /*
-// // Definition for a Node.
-// class Node {
-//     public int val;
-//     public List<Node> neighbors;
-//     public Node() {
-//         val = 0;
-//         neighbors = new ArrayList<Node>();
-//     }
-//     public Node(int _val) {
-//         val = _val;
-//         neighbors = new ArrayList<Node>();
-//     }
-//     public Node(int _val, ArrayList<Node> _neighbors) {
-//         val = _val;
-//         neighbors = _neighbors;
-//     }
-// }
-// */
-
-// class Solution {
-//     public Node cloneGraph(Node node) {
-//         if(node==null){
-//             return null;
-//         }
-
-//         HashMap<Node,Node>map=new HashMap<>();
-//         Queue<Node>qq=new LinkedList<>();
-
-//         Node clone=new Node(node.val);
-//         map.put(node,clone);
-//         qq.offer(node);
-
-//         while(!qq.isEmpty()){
-//             Node curr=qq.poll();
-
-//             for(Node nn:curr.neighbors){
-                
-//                 if(!map.containsKey(nn)){
-//                     map.put(nn,new Node(nn.val));
-//                     qq.offer(nn);
-//                 }
-
-//                 map.get(curr).neighbors.add(map.get(nn));
-
-//             }
-//         }
-//       return  map.get(node);
-//     }
-// }
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
 
 class Solution {
-
-    HashMap<Node, Node> map = new HashMap<>();
-
+    HashMap<Node,Node>mp;
     public Node cloneGraph(Node node) {
-        if (node == null) return null;
-
-        return dfs(node);
+        if(node==null){
+            return null;
+        }
+        mp=new HashMap<>();
+        return solve(node);
     }
-
-    private Node dfs(Node node) {
-
-       
-        if (map.containsKey(node)) {
-            return map.get(node);
+    public Node solve(Node node){
+        if(mp.containsKey(node)){
+            return mp.get(node);
+        }
+        Node clone=new Node(node.val);
+        mp.put(node,clone);
+        for(Node nn:node.neighbors){
+            mp.get(node).neighbors.add(solve(nn));
         }
 
-        Node clone = new Node(node.val);
-        map.put(node, clone);
-
-      
-        for (Node nei : node.neighbors) {
-            clone.neighbors.add(dfs(nei));
-        }
-
-        return clone;
+        return mp.get(node);
     }
 }
