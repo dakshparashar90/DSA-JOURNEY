657eaa50b1e42b4483be5bbca5c8086e39daf838
dakshparashar90 <155139713+dakshparashar90@users.noreply.github.com>
2026-09-12 23:37:33 +0530

    Time: 1 ms (3.69%), Space: 46.8 MB (14.28%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -12,24 +12,71 @@ class Node {
     }
 }
 */
-
-class Solution {
-    HashMap<Node, Node> map = new HashMap<>();
-
-    public Node copyRandomList(Node head) {
+class Solution{
+    public Node copyRandomList(Node head){
         if (head == null) return null;
 
-        if (map.containsKey(head)) {
-            return map.get(head);
+        HashMap<Node,Node>map=new HashMap<>();
+
+        Queue<Node> qq=new LinkedList<>();
+
+        Node newNode=new Node(head.val);
+        map.put(head,newNode);
+        qq.offer(head);
+
+        while(!qq.isEmpty()){
+            Node curr=qq.poll();
+
+            if(curr.next!=null){
+
+                if(!map.containsKey(curr.next)){
+                    map.put(curr.next,new Node(curr.next.val));
+                    qq.offer(curr.next);
+                }
+
+                map.get(curr).next = map.get(curr.next);
+            }
+             if (curr.random != null) {
+
+                if (!map.containsKey(curr.random)) {
+                    map.put(curr.random, new Node(curr.random.val));
+                    qq.offer(curr.random);
+                }
+
+                map.get(curr).random = map.get(curr.random);
+            }
         }
 
-        Node newNode = new Node(head.val);
-
-        map.put(head, newNode);
-
-        newNode.next = copyRandomList(head.next);
-        newNode.random = copyRandomList(head.random);
-
         return newNode;
+        
+
     }
 }
+
+
+
+
+
+
+
+
+// class Solution {
+//     HashMap<Node, Node> map = new HashMap<>();
+
+//     public Node copyRandomList(Node head) {
+//         if (head == null) return null;
+
+//         if (map.containsKey(head)) {
+//             return map.get(head);
+//         }
+
+//         Node newNode = new Node(head.val);
+
+//         map.put(head, newNode);
+
+//         newNode.next = copyRandomList(head.next);
+//         newNode.random = copyRandomList(head.random);
+
+//         return newNode;
+//     }
+// }
