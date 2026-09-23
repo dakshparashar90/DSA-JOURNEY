24f63095228817ead011ee51337f1f271197900b
dakshparashar90 <155139713+dakshparashar90@users.noreply.github.com>
2026-09-23 19:37:14 +0530

    Time: 35 ms (72.01%), Space: 64.4 MB (9.91%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -7,7 +7,7 @@ class Solution {
             dp[i][i]=1;
       }
 
-      for(int i=s.length()-1;i>=0;i--){
+      for(int i=s.length()-2;i>=0;i--){
             for(int j=i+1;j<s.length();j++){
                 if(s.charAt(i)==s.charAt(j)){
                      dp[i][j]= 2+dp[i+1][j-1];
