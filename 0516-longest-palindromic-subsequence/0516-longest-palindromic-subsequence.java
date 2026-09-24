a6c6c6ae0c68c6a7e4e82c8e40af7bdbbc1ca25e
dakshparashar90 <155139713+dakshparashar90@users.noreply.github.com>
2026-09-24 06:34:16 +0530

    Time: 27 ms (85.17%), Space: 42.9 MB (98.66%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,53 +1,29 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-   
-    int dp[][]=new int[s.length()][s.length()];
-      
-      for(int i=0;i<s.length();i++){
-            dp[i][i]=1;
-      }
 
-      for(int i=s.length()-2;i>=0;i--){
-            for(int j=i+1;j<s.length();j++){
-                if(s.charAt(i)==s.charAt(j)){
-                     dp[i][j]= 2+dp[i+1][j-1];
-                 }
+        int n = s.length();
+        int[] dp = new int[n];
 
-                 else{
-                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j-1]);
-                 }
+        Arrays.fill(dp, 1);
+
+        for (int i = n - 2; i >= 0; i--) {
+
+            int prev = 0;
+
+            for (int j = i + 1; j < n; j++) {
+
+                int temp = dp[j];
+
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[j] = 2 + prev;
+                } else {
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
+                }
+
+                prev = temp;
+            }
         }
-      }
-      return dp[0][s.length()-1];
-      
+
+        return dp[n - 1];
     }
 }
-
-
-// class Solution {
-//     int dp[][];
-//     public int solve(String s,int i,int j){
-//         if(i>j){
-//             return 0;
-//         }
-//         if(i==j){
-//             return dp[i][j]= 1;
-//         }
-//         if(dp[i][j]!=-1){
-//             return dp[i][j];
-//         }
-
-//         if(s.charAt(i)==s.charAt(j)){
-//             return dp[i][j]= 2+solve(s,i+1,j-1);
-//         }
-
-//         return dp[i][j]= Math.max(solve(s,i+1,j),solve(s,i,j-1));
-//     }
-//     public int longestPalindromeSubseq(String s) {
-//         dp=new int[s.length()+1][s.length()+1];
-//         for(int i=0;i<s.length();i++){
-//             Arrays.fill(dp[i],-1);
-//         }
-//         return solve(s,0,s.length()-1);
-//     }
-// }
